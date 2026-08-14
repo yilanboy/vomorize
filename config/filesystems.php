8b4a3b0c160670c4f76d36aebc3723e2baaa4270
allenjiang <allenjianggo@gmail.com>
2026-08-14 21:50:32 +0800

    fix(config): wrong Cloudflare R2 config

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
             'driver' => 's3',
             'key' => env('R2_ACCESS_KEY_ID'),
             'secret' => env('R2_SECRET_ACCESS_KEY'),
-            'region' => env('R2_REGION'),
+            'region' => 'auto',
             'bucket' => env('R2_BUCKET'),
             'url' => env('R2_URL'),
             'endpoint' => env('R2_ENDPOINT'),
